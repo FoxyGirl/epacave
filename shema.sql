4df34d5a256b982cf3bc12b4c0f463e9c3b60d72
Elena Pavlova <lenlho22@gmail.com>
2017-05-23 21:02:26 +0300

    Create Data Base for epacave

---
+++ shema.sql
@@ -0,0 +1,52 @@
+CREATE TABLE `users` (
+	`id` int NOT NULL,
+	`user_name` varchar(50) NOT NULL,
+	`dt_registry` DATETIME NOT NULL,
+	`email` varchar(64) NOT NULL,
+	`password` varchar(64) NOT NULL,
+	`avatar_path` varchar(100) NOT NULL,
+	`contacts` varchar(100) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `lots` (
+	`id` int NOT NULL,
+	`lot_name` varchar(50) NOT NULL,
+	`user_id` int NOT NULL,
+	`dt_add` DATETIME NOT NULL,
+	`description` varchar(255) NOT NULL,
+	`img_path` varchar(255) NOT NULL,
+	`start_price` int NOT NULL,
+	`dt_close` DATETIME NOT NULL,
+	`step_bet` int NOT NULL,
+	`fav_count` int NOT NULL,
+	`category_id` int NOT NULL,
+	`winner_user_id` int NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `categories` (
+	`id` int NOT NULL,
+	`name` char(255) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `bets` (
+	`id` int NOT NULL,
+	`dt_add` DATETIME NOT NULL,
+	`bet` int NOT NULL,
+	`user_id` int NOT NULL,
+	`lot_id` int NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+ALTER TABLE `lots` ADD CONSTRAINT `lots_fk0` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`);
+
+ALTER TABLE `lots` ADD CONSTRAINT `lots_fk1` FOREIGN KEY (`category_id`) REFERENCES `categories`(`id`);
+
+ALTER TABLE `lots` ADD CONSTRAINT `lots_fk2` FOREIGN KEY (`winner_user_id`) REFERENCES `users`(`id`);
+
+ALTER TABLE `bets` ADD CONSTRAINT `bets_fk0` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`);
+
+ALTER TABLE `bets` ADD CONSTRAINT `bets_fk1` FOREIGN KEY (`lot_id`) REFERENCES `lots`(`id`);
+
